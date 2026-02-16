7c2bbf48978a801292f2e746675b4256d3b6c90e
Igris2810 <shavyo100@gmail.com>
2026-02-17 04:01:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function Navbar(){
   return (
     <div style={{display:"flex",justifyContent:"space-between",padding:"18px 40px",background:"white",boxShadow:"0 6px 20px rgba(0,0,0,0.08)",position:"sticky",top:0,zIndex:9}}>
     <div style={{display:"flex",alignItems:"center",gap:10}}>
-  <img src="/logo.jpeg" style={{height:40}} />
+  <img src="/logo.jpeg" style={{height:70}} />
   <div>
     <div style={{fontWeight:800,color:"#0B3D91",fontSize:20}}>My Bima Mitra</div>
     <div style={{fontSize:12,color:"#0b5ed7"}}>Insurance Advisor</div>
